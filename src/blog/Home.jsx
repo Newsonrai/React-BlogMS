b6e7ce512573679e7771d09557e6e32c5b6b6085
Newson Rai <firstnepseking@gmail.com>
2024-12-23 22:42:13 +0545

    blog files and other files

--- src/blog/Home.jsx
+++ src/blog/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+import Layout from '../components/navbar/layout/Layout'
+import Card from './component/card/Card'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Layout>
+        <div className='flex flex-wrap justify-center space-x-5 mt-5'>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        </div>
+    </Layout>
   )
 }
 
